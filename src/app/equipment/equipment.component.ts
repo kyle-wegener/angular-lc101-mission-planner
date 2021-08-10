bddffeadc20aa067cf42c5f1934cc9d5988f297b
kwage <kylewegener@gmail.com>
2021-08-09 22:10:58 -0400

    Finished entire studio

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: string[] = ["Habitat dome", "Drones", "Food containers", "Oxygen tanks"];
+
+  equipmentBeingEdited: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(equipmentName: string) {
+    this.equipment.push(equipmentName);
+  }
+
+  remove(equipmentName: string) {
+    let index = this.equipment.indexOf(equipmentName);
+    this.equipment.splice(index, 1);
+  }
+  
+  edit(equip: string) {
+    this.equipmentBeingEdited = equip;
+  }
+
+  save(oldEquipmentName: string, newEquipmentName: string) {
+    let index = this.equipment.indexOf(oldEquipmentName);
+    console.log(index);
+    this.equipment[index] = newEquipmentName;
+    this.equipmentBeingEdited = null;
   }
 
 }
